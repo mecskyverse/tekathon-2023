362f7f788d8046df06ddf137e13fb6afceaff69f
Kshitij Chauhan <99241138+KshitijChauhanX@users.noreply.github.com>
2023-09-21 06:21:48 +0530

    Update Timer.js

--- client/src/components/Timer.js
+++ client/src/components/Timer.js
@@ -1,1 +1,7 @@
+export default function Timer(props) {
 
+    return (
+      <div className="header__timer text-white text-3xl text- w-36 h-16 flex justify-center items-center">
+        <b>{props.minutes<10 ? "0"+ props.minutes : props.minutes}:{props.seconds<10 ? "0"+props.seconds : props.seconds}</b>
+      </div>)
+}
